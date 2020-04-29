935aa06124b403c9c8e998cd51f1149062a1b440
Andrius Degutis <andriusdgt@gmail.com>
2020-04-29 20:47:46 +0300

    made final by default

--- src/main/java/com/andriusdgt/thedots/core/exception/DuplicatePointException.java
+++ src/main/java/com/andriusdgt/thedots/core/exception/DuplicatePointException.java
@@ -1,6 +1,6 @@
 package com.andriusdgt.thedots.core.exception;
 
-public class DuplicatePointException extends RuntimeException {
+public final class DuplicatePointException extends RuntimeException {
 
     public DuplicatePointException() {
         super("Duplicate Point provided");
