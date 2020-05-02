c8fe37878a48bf194268bd5a5dd32b124b7cb047
Andrius Degutis <andriusdgt@gmail.com>
2020-05-03 01:53:50 +0300

    removed unused method

--- src/main/java/com/andriusdgt/thedots/core/repository/PointListRepository.java
+++ src/main/java/com/andriusdgt/thedots/core/repository/PointListRepository.java
@@ -3,13 +3,10 @@ package com.andriusdgt.thedots.core.repository;
 import com.andriusdgt.thedots.core.model.PointList;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PointListRepository {
 
     void save(PointList pointList);
-
-    Optional<PointList> findById(String id);
 
     PointList findByName(String name);
 
